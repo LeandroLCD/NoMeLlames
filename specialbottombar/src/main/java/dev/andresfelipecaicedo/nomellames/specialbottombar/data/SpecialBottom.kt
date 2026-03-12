815fb4670ddaaaa2ac14a018cd9fc9008b2a165f
Leandro Colmenarez Diaz <leandrojcolmenarezd@gmail.com>
2026-03-12 08:58:10 -0300

    Initialize project structure with basic setup and components for SpecialBottomBar

---
+++ specialbottombar/src/main/java/dev/andresfelipecaicedo/nomellames/specialbottombar/data/SpecialBottom.kt
@@ -0,0 +1,39 @@
+package dev.andresfelipecaicedo.nomellames.specialbottombar.data
+
+import androidx.annotation.DrawableRes
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+sealed class SpecialBottom {
+
+    data class Badge(
+        val backgroundColor: Color = Color.White,
+        val textColor: Color = Color.Black,
+        val text: String? = null
+    ) : SpecialBottom()
+
+    data class Id(val key: String) : SpecialBottom()
+    
+    data class Item(
+        @DrawableRes val icon: Int,
+        @DrawableRes val activatedIcon: Int,
+        val tag: String,
+        val id: Id,
+        val badge: Badge? = null
+    ) : SpecialBottom()
+
+    data class Theme(
+        val backgroundColor: Color = Color.White,
+        val selectedColor: Color = Color.Black,
+        val unselectedColor: Color = Color.Gray,
+        val shadowColor: Color = selectedColor,
+        val spread: Dp = 8.dp,
+        val blurRadius: Dp = 16.dp,
+        val borderRadius: Dp = 32.dp,
+        val startAnimation: Boolean = true,
+        @DrawableRes val iconPlus: Int? = null,
+        val backGroundSecondaryColor: Color = backgroundColor,
+    ) : SpecialBottom()
+}
+
