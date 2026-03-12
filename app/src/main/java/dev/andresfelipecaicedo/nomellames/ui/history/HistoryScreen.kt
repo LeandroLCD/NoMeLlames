b61c76c1a15c6a7e3ebba2e4d745174dd83c0998
Leandro Colmenarez Diaz <leandrojcolmenarezd@gmail.com>
2026-03-12 09:25:17 -0300

    Update HistoryScreen and HistoryUiState for Hilt integration and clean up state management

--- app/src/main/java/dev/andresfelipecaicedo/nomellames/ui/history/HistoryScreen.kt
+++ app/src/main/java/dev/andresfelipecaicedo/nomellames/ui/history/HistoryScreen.kt
@@ -49,7 +49,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import dev.andresfelipecaicedo.nomellames.R
 import dev.andresfelipecaicedo.nomellames.domain.model.HistoryItem
 import dev.andresfelipecaicedo.nomellames.domain.useCase.history.IGetCallHistoryUseCase
