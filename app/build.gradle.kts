c487697df46e506159a45d90995facacbf87a43f
LeandroLCD <leandrojcolmenarezd@gmail.com>
2026-03-23 13:23:23 -0300

    Update string resources for theme options and translations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "cl.blipblipcode.prefixsapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
